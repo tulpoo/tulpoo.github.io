69cf1b7948683902d1391e5ea695f8fd641d3759
Victor Pham <vietpv94@gmail.com>
2021-12-02 12:11:55 +0700

    dev

--- 1.js
+++ 1.js
@@ -1,1 +1,1 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{771:function(n,e,a){"use strict";a.r(e);var t=a(0),r=a.n(t),u=a(7);e.default=function(){return r.a.createElement(r.a.Fragment,null,r.a.createElement(u.a,null))}}}]);
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{811:function(n,e,a){"use strict";a.r(e);var t=a(0),r=a.n(t),u=a(10);e.default=function(){return r.a.createElement(r.a.Fragment,null,r.a.createElement(u.a,null))}}}]);
