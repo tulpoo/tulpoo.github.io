94dd01eeb002542853481583d078dbdea98a3e1a
Victor Pham <vietpv94@gmail.com>
2021-12-07 16:22:38 +0700

    up

--- 1.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{811:function(n,e,a){"use strict";a.r(e);var t=a(0),r=a.n(t),u=a(10);e.default=function(){return r.a.createElement(r.a.Fragment,null,r.a.createElement(u.a,null))}}}]);
